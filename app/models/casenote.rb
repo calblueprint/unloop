6e5b7d0d1238e23da7f7f4844736380a3e939725
Frederick Kim <freddy.kim8@gmail.com>
2019-10-03 21:28:35 -0700

    Rails generate casework model

---
+++ app/models/casenote.rb
@@ -0,0 +1,3 @@
+class Casenote < ApplicationRecord
+  belongs_to :staff
+end
---
+++ db/migrate/20191004042135_create_casenotes.rb
@@ -0,0 +1,12 @@
+class CreateCasenotes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :casenotes do |t|
+      t.string :description
+      t.boolean :internal
+      t.references :staff, null: false, foreign_key: true
+      t.string :title
+
+      t.timestamps
+    end
+  end
+end
