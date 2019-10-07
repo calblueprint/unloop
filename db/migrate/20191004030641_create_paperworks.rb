0f7a05f398c1e7b39331e45a8321e4bd764159d9
Frederick Kim <freddy.kim8@gmail.com>
2019-10-06 20:33:48 -0700

    Change has_one to belongs_to participant

--- db/migrate/20191004030641_create_paperworks.rb
+++ db/migrate/20191004030641_create_paperworks.rb
@@ -2,8 +2,9 @@ class CreatePaperworks < ActiveRecord::Migration[6.0]
   def change
     create_table :paperworks do |t|
       t.references :staff, null: false, foreign_key: true
-      t.string :link
-      t.string :title
+      t.references :participant, null: false, foreign_key: true
+      t.string :link, null: false
+      t.string :title, null: false
       t.boolean :agree
 
       t.timestamps
