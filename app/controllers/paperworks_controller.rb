2578014f9d122d725489677303f8ff15e03e96eb
Frederick Kim <freddy.kim8@gmail.com>
2019-10-13 16:41:05 -0700

    Move all API calls under API namespace

--- app/controllers/paperworks_controller.rb
+++ app/controllers/paperworks_controller.rb
@@ -11,54 +11,7 @@ class PaperworksController < ApplicationController
     @paperwork = Paperwork.new
   end
 
-  def create
-    @paperwork = Paperwork.new(paperwork_params)
-    saved = @paperwork.save
-    if saved
-      redirect_to @paperwork
-    else
-      render 'new'
-    end
-  end
-
   def edit
     @paperwork = Paperwork.find(params[:id])
-  end
-
-  def update
-    @paperwork = Paperwork.find(params[:id])
-    if @paperwork.update(paperwork_params)
-      redirect_to @paperwork
-    else
-      render 'edit'
-    end
-  end
-
-  def complete
-    @paperwork = Paperwork.find(params[:id])
-    respond_to do |format|
-      if @paperwork.update(agree: true)
-        format.html { redirect_to @paperwork, notice: 'Paperwork has been marked as agreed.' }
-        format.json { render :show, status: :ok, location: @paperwork }
-      else
-        format.html { render :new }
-        format.json { render json: @paperwork.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def destroy
-    @paperwork = Paperwork.find(params[:id])
-    @paperwork.destroy
-    
-    redirect_to paperworks_path
-  end
-
-  private
-
-  def paperwork_params
-    paperwork_param = params.require(:paperwork).permit(:title, :link, :agree)
-    # TODO: Make sure to specify staff and participant
-    paperwork_param.merge(staff_id: 1, participant_id: 1)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,12 @@ Rails.application.routes.draw do
 
   # On failed authorization redirect to sign in page
   get 'auth/failure', to: redirect('/omniusers/sign_in')
-  resources :paperworks
+
+  resources :paperworks, only: [:index, :show, :new, :edit]
+
+  namespace :api, defaults: { format: :json } do
+    resources :paperworks, only: [:create, :update, :destroy] do
+      patch 'complete', to: 'paperworks#complete'
+    end
+  end
 end
