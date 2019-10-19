53bd159f48f89e2d9a4406704f3e510b8f0f3f8c
Julian Kung <julianrkung@berkeley.edu>
2019-10-19 16:10:46 -0700

    Made agree default false

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_12_044313) do
+ActiveRecord::Schema.define(version: 2019_10_19_230508) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(version: 2019_10_12_044313) do
   create_table "paperworks", force: :cascade do |t|
     t.string "link"
     t.string "title"
-    t.boolean "agree"
+    t.boolean "agree", default: false
     t.bigint "staff_id", null: false
     t.bigint "participant_id", null: false
     t.datetime "created_at", precision: 6, null: false
