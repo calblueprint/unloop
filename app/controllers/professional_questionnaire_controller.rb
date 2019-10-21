756f7b72a8777edde3551b20d273b227936a86a1
didvi <divi@berkeley.edu>
2019-10-20 21:01:15 -0700

    added questionnaire controllers

---
+++ app/controllers/professional_questionnaire_controller.rb
@@ -0,0 +1,50 @@
+class ProfessionalQuestionnaireController < ApplicationController
+  def index
+    @questionnaires = ProfessionalQuestionnaire.all
+  end
+  
+  def show
+    @questionnaire = ProfessionalQuestionnaire.find(params[:id])
+  end
+
+  def new
+    @questionnaire = ProfessionalQuestionnaire.new
+  end
+
+  def create
+    @questionnaire = ProfessionalQuestionnaire.new(questionnaire_params)
+
+    saved = @questionnaire.save
+    if saved
+      redirect_to @questionnaire
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @questionnaire = ProfessionalQuestionnaire.find(params[:id])
+    if @questionnaire.update(questionnaire_params)
+      redirect_to @questionnaire
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @questionnaire = ProfessionalQuestionnaire.find(params[:id])
+    @questionnaire.destroy
+    
+    # may not work
+    redirect_to professional_questionnaire_path
+  end
+
+  private
+
+  # may not work
+  def questionnaire_params
+    questionnaire_params = params.require(:questionnaire).permit(:participant_id, :course_completion,
+      :work_history, :job_search_materials, :professional_goals, :skills_assessment_date, 
+      :barriers, :mentorship_interest,:success_strategies)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  
+  resources :professional_questionnaire
+  resources :personal_questionnaire
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # Routes for Google authentication
