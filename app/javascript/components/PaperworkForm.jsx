a5dd414773554f2f228c321a9de2ebcc29166413
Alison Dowski <adowski@berkeley.edu>
2019-10-27 17:53:47 -0700

    cleaned up import statements

--- app/javascript/components/PaperworkForm.jsx
+++ app/javascript/components/PaperworkForm.jsx
@@ -1,12 +1,7 @@
 import React from "react";
 import Button from '@material-ui/core/Button';
 import '../../assets/stylesheets/paperworks.scss';
-import TextField from '@material-ui/core/TextField';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
+import { TextField, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@material-ui/core/';
 
 
 class PaperworkForm extends React.Component {
