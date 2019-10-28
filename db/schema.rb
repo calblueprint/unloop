527c907e5ade75e9c62e8f56460026383e6f1fff
Frederick Kim <freddy.kim8@gmail.com>
2019-10-28 02:17:23 -0700

    Create admin role under Omniuser
    Shorten route for admin
    
    Change admin to role under Omniuser
    
    * Removed admin user model
    * Added admin boolean under Omniuser
    * Updated the seed to reflect new admin user
    * Added enum to user_type under Omniuser
    * Set default participant status to r0
    * Added delegation for email on Staff and Participant

--- db/schema.rb
+++ db/schema.rb
@@ -15,44 +15,6 @@ ActiveRecord::Schema.define(version: 2019_10_31_201428) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "active_admin_comments", force: :cascade do |t|
-    t.string "namespace"
-    t.text "body"
-    t.string "resource_type"
-    t.bigint "resource_id"
-    t.string "author_type"
-    t.bigint "author_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
-    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
-    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
-  end
-
-  create_table "admin_users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_admin_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
-  end
-
-  create_table "admins", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_admins_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
-  end
-
   create_table "casenotes", force: :cascade do |t|
     t.string "description"
     t.boolean "internal"
@@ -84,7 +46,7 @@ ActiveRecord::Schema.define(version: 2019_10_31_201428) do
   create_table "paperworks", force: :cascade do |t|
     t.string "link"
     t.string "title"
-    t.boolean "agree"
+    t.boolean "agree", default: false
     t.bigint "staff_id", null: false
     t.bigint "participant_id", null: false
     t.datetime "created_at", precision: 6, null: false
