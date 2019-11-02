37ff9e51cb650253f8458625e97875ad645daa07
Frederick Kim <freddy.kim8@gmail.com>
2019-11-02 12:59:10 -0700

    Change policies to reflect use_type enum change
    Change create paperwork condition

--- app/policies/casenote_policy.rb
+++ app/policies/casenote_policy.rb
@@ -1,48 +1,49 @@
 class CasenotePolicy < ApplicationPolicy
-    def isStaff?
-      user.present? && user.user_type == "Staff"
-    end
+  def index?
+    staff?
+  end
 
-    def index?
-      isStaff?
-    end
+  def create?
+    staff?
+  end
 
-    def create?
-      isStaff?
-    end
-  
-    def update?
-      isStaff?
-    end
-  
-    def destroy?
-      isStaff?
-    end
+  def update?
+    staff?
+  end
 
-    def show?
-      create? or (user.user_type == "Participant" && user.participant.id == resource.participant_id && resource.internal == false)
-    end
+  def destroy?
+    staff?
+  end
 
-    def internal
-      isStaff?
-    end
+  def show?
+    create? || (user.participant? && user.participant.id == resource.participant_id && resource.internal == false)
+  end
 
-    def set_casenote?
-      show?
-    end
+  def internal
+    staff?
+  end
 
-    class Scope < Scope
+  def set_casenote?
+    show?
+  end
+
+  class Scope < Scope
 #         The first argument is a user. In your controller, Pundit will call the current_user method 
 #               to retrieve what to send into this argument.
 #         The second argument is a scope of some kind on which to perform some kind of query. 
 #               It will usually be an ActiveRecord class or a ActiveRecord::Relation.
-      def resolve
-        if user.user_type == "Staff"
-            scope.all
-        else
-            scope.where([participant_id: user.participant.id, internal: false])
-        end
+    def resolve
+      if user.staff?
+          scope.all
+      else
+          scope.where([participant_id: user.participant.id, internal: false])
       end
     end
   end
-  
+
+  private
+
+  def staff?
+    user.present? && user.staff?
+  end
+end
--- app/policies/paperwork_policy.rb
+++ app/policies/paperwork_policy.rb
@@ -1,52 +1,53 @@
 class PaperworkPolicy < ApplicationPolicy
-    def isStaff?
-      user.present? && user.user_type == "Staff"
-    end
+  def index?
+    staff?
+  end
 
-    def index?
-      isStaff?
-    end
-    
-    def create?
-      isStaff?
-    end
-  
-    def destroy?
-      isStaff?
-    end
+  def create?
+    staff?
+  end
 
-    def complete?
-      isStaff?
-    end
+  def destroy?
+    staff?
+  end
 
-    def show?
-        isStaff? or (user.user_type == "Participant" && user.participant.id == resource.participant_id)
-    end
+  def complete?
+    staff?
+  end
 
-    def update?
-      show?
-    end
-    
-    def set_paperwork?
-      show?
-    end
+  def show?
+    staff? || (user.participant? && user.participant.id == resource.participant_id)
+  end
 
-    def agree?
-        user.user_type == "Participant" && (user.participant.id == resource.participant_id)
-    end
+  def update?
+    show?
+  end
 
-    class Scope < Scope
-#         The first argument is a user. In your controller, Pundit will call the current_user method 
-#               to retrieve what to send into this argument.
-#         The second argument is a scope of some kind on which to perform some kind of query. 
-#               It will usually be an ActiveRecord class or a ActiveRecord::Relation.
-        def resolve
-            if user.user_type == "Staff"
-                scope.all
-            else
-                scope.where(participant_id: user.participant.id)
-            end
-        end
+  def set_paperwork?
+    show?
+  end
+
+  def agree?
+    user.participant? && (user.participant.id == resource.participant_id)
+  end
+
+  class Scope < Scope
+  # The first argument is a user. In your controller, Pundit will call the current_user method 
+  # to retrieve what to send into this argument.
+  # The second argument is a scope of some kind on which to perform some kind of query. 
+  # It will usually be an ActiveRecord class or a ActiveRecord::Relation.
+    def resolve
+      if user.staff?
+        scope.all
+      else
+        scope.where(participant_id: user.participant.id)
+      end
     end
   end
-  
+
+  private
+
+  def staff?
+    user.present? && user.staff?
+  end
+end
