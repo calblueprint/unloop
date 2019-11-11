b460118f32696d4de775d81dcf6003745f090c71
didvi <divi@berkeley.edu>
2019-11-10 21:23:16 -0800

    added creation of questionnaires if not already created

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -3,8 +3,19 @@ class ParticipantsController < ApplicationController
     @participant = authorize Participant.find(params[:id])
     @paperworks = @participant.paperworks
     @case_notes = @participant.case_notes
-    @personal_questionnaire = @participant.personal_questionnaire
-    @professional_questionnaire = @participant.professional_questionnaire
+
+    if @participant.personal_questionnaire.nil?
+      @personal_questionnaire = PersonalQuestionnaire.create("participant_id": @participant.id)
+    else
+      @personal_questionnaire = @participant.personal_questionnaire
+    end
+
+    if @participant.professional_questionnaire.nil?
+      @professional_questionnaire = ProfessionalQuestionnaire.create("participant_id": @participant.id)
+    else
+      @professional_questionnaire = @participant.personal_questionnaire
+    end
+    
   end
 
   def dashboard
