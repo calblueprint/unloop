b61ced4d92eb5ab98a88bf01b723ff2502ee8306
Frederick Kim <freddy.kim8@gmail.com>
2019-11-11 13:07:42 -0800

    Update webpack and eslint config
    Changed default importing of react components into Rails views to containers instead of components
    Changed webpacker.yml to include assets folder for absolute importing
    Updated eslint config to fit our project

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,13 +1,13 @@
+/* eslint-disable import/no-unresolved */
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-
+require('@rails/ujs').start();
+require('turbolinks').start();
+require('@rails/activestorage').start();
+require('channels');
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -16,6 +16,6 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 // Support component names relative to this directory:
-var componentRequireContext = require.context("components", true);
-var ReactRailsUJS = require("react_ujs");
-ReactRailsUJS.useContext(componentRequireContext);
+const containerRequireContext = require.context('containers', true);
+const ReactRailsUJS = require('react_ujs');
+ReactRailsUJS.useContext(containerRequireContext);
