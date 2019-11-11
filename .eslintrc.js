cfc95dfef270015659bc0923e6443b171a1b5262
Frederick Kim <freddy.kim8@gmail.com>
2019-11-11 13:07:42 -0800

    Update webpack and eslint config
    Changed default importing of react components into Rails views to containers instead of components
    Changed webpacker.yml to include assets folder for absolute importing
    Updated eslint config to fit our project

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ const prettierOptions = JSON.parse(
 module.exports = {
   parser: 'babel-eslint',
   extends: ['airbnb', 'prettier', 'prettier/react'],
-  plugins: ['prettier', 'redux-saga', 'react', 'react-hooks', 'jsx-a11y'],
+  plugins: ['prettier', 'react', 'react-hooks', 'jsx-a11y'],
   env: {
     jest: true,
     browser: true,
@@ -71,18 +71,17 @@ module.exports = {
     'react/jsx-filename-extension': 0,
     'react/jsx-no-target-blank': 0,
     'react/jsx-uses-vars': 2,
+    'react/jsx-props-no-spreading': 0,
     'react/require-default-props': 0,
     'react/require-extension': 0,
     'react/self-closing-comp': 0,
     'react/sort-comp': 0,
-    'redux-saga/no-yield-in-race': 2,
-    'redux-saga/yield-effects': 2,
     'require-yield': 0,
   },
   settings: {
     'import/resolver': {
       webpack: {
-        config: './internals/webpack/webpack.prod.babel.js',
+        config: 'config/webpack/production.js',
       },
     },
   },
