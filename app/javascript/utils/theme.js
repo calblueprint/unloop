9bb1b40506c2970363e0434d6b91af381204b44d
Frederick Kim <freddy.kim8@gmail.com>
2019-11-14 15:41:22 -0800

    Add theme provider

---
+++ app/javascript/utils/theme.js
@@ -0,0 +1,11 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+export const theme = createMuiTheme({
+  palette: {
+    common: {
+      blue: '#187be5',
+    },
+  },
+});
+
+export default theme;
