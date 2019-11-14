a318f334f8eaede6ee1a34a852c2ae4e73d8f6cb
kyuwonk <freddy.kim8@gmail.com>
2019-11-14 11:51:13 -0800

    Temp commit

---
+++ app/javascript/containers/ParticipantShowPage/styles.js
@@ -0,0 +1,13 @@
+/*
+ * ParticipantShowPage Styles
+ *
+ * This contains all the styles for the ParticipantShowPage container.
+ */
+
+export const styles = {
+  root: {
+    backgroundColor: '#000000',
+  },
+};
+
+export default styles;
---
+++ app/javascript/containers/StaffDashboardPage/styles.js
@@ -0,0 +1,13 @@
+/*
+ * StaffDashboardPage Styles
+ *
+ * This contains all the styles for the StaffDashboardPage container.
+ */
+
+export const styles = {
+  root: {
+    backgroundColor: '#000000',
+  },
+};
+
+export default styles;
