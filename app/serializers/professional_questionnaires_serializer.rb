ebf6a0894e5e7d05c33f1b857910b46845e9feba
Frederick Kim <freddy.kim8@gmail.com>
2019-11-28 15:34:58 -0800

    Remove staff relationship comment from questionnaire serializer

--- app/serializers/professional_questionnaires_serializer.rb
+++ app/serializers/professional_questionnaires_serializer.rb
@@ -3,7 +3,6 @@ class ProfessionalQuestionnairesSerializer < ActiveModel::Serializer
   :work_history, :job_search_materials, :professional_goals, :skills_assessment_date, 
   :barriers, :mentorship_interest,:success_strategies, :participant
 
-  # belongs_to :staff, serializer: SimpleStaffSerializer
   belongs_to :participant, serializer: SimpleParticipantSerializer
-  end
+end
   
