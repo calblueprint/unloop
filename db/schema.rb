6f41c118788b4263634eb9df8e8b8d48b2709e6e
Julian Kung <julianrkung@berkeley.edu>
2020-02-28 17:00:36 -0800

    Created ActionItems model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_07_120858) do
+ActiveRecord::Schema.define(version: 2020_02_29_004029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "action_items", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.boolean "is_template"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "case_notes", force: :cascade do |t|
     t.string "description"
