3e724dc3966c60b8b6a5ab518714a7e0c5ff9428
Julian Kung <julianrkung@berkeley.edu>
2020-03-08 19:12:29 -0700

    Removed commented code

--- db/migrate/20200301015408_add_assocation_between_assignments_and_users.rb
+++ db/migrate/20200301015408_add_assocation_between_assignments_and_users.rb
@@ -5,7 +5,5 @@ class AddAssocationBetweenAssignmentsAndUsers < ActiveRecord::Migration[6.0]
 
     add_reference :assignments, :assigned_to, references: :users, index: true
     add_foreign_key :assignments, :users, column: :assigned_to_id
-    # add_reference :assignments, :assigned_by, foreign_key: { to_table: 'Staff'}
-    # add_reference :assignments, :assigned_to, foreign_key: { to_table: 'User' }
   end
 end
