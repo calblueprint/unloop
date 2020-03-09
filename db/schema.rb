24191feb7baa63b00007cfb5dae2634a732510d8
Erin <erinsong@berkeley.edu>
2020-03-08 21:07:09 -0700

    remigrated and reseeded database

--- db/schema.rb
+++ db/schema.rb
@@ -94,8 +94,6 @@ ActiveRecord::Schema.define(version: 2020_03_07_003647) do
   end
 
   create_table "studio_assessments", force: :cascade do |t|
-    t.string "name"
-    t.bigint "participant_id"
     t.integer "bigpicture_score"
     t.string "bigpicture_comment"
     t.integer "progfundamentals_score"
@@ -114,10 +112,13 @@ ActiveRecord::Schema.define(version: 2020_03_07_003647) do
     t.string "problemsolvingalt_comment"
     t.boolean "passed_capstone"
     t.string "capstone_comment"
-    t.string "proctor"
     t.string "assessment_type"
+    t.bigint "staff_id", null: false
+    t.bigint "participant_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["participant_id"], name: "index_studio_assessments_on_participant_id"
+    t.index ["staff_id"], name: "index_studio_assessments_on_staff_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -146,4 +147,6 @@ ActiveRecord::Schema.define(version: 2020_03_07_003647) do
   add_foreign_key "personal_questionnaires", "participants"
   add_foreign_key "professional_questionnaires", "participants"
   add_foreign_key "staffs", "users"
+  add_foreign_key "studio_assessments", "participants"
+  add_foreign_key "studio_assessments", "staffs"
 end
