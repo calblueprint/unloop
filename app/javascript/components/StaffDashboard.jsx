4f6cb13269b8c7ddab38b4be9da82d8e2ce23941
Kyle Hua <kyleqhua@gmail.com>
2020-03-08 18:53:38 -0700

    temporary frontend up

--- app/javascript/components/StaffDashboard.jsx
+++ app/javascript/components/StaffDashboard.jsx
@@ -1,85 +1,49 @@
 import React from 'react';
-import InputBase from '@material-ui/core/InputBase';
-import IconButton from '@material-ui/core/IconButton';
-import SearchIcon from '@material-ui/icons/Search';
-
 import ParticipantCard from './ParticipantCard';
 import Navbar from 'components/Navbar';
 import PropTypes from 'prop-types';
-import { Grid } from '@material-ui/core';
 
-const TrieSearch = require('trie-search');
+let action = {
+      title: "Finish Sprint",
+      description: "Complete the sprint by tonight please. Thank you",
+      due: new Date(),
+      category: "code",
+}
 
 class StaffDashboard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      participants: this.props.participants,
+      assignments: this.props.assignments,
     };
     this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
-    const { participants } = this.props;
-    const trie = new TrieSearch('name');
-    trie.addAll(participants);
-    this.setState({
-      trie,
-    });
+   
   }
 
   handleChange(e) {
-    const searchVal = e.target.value;
-    if (searchVal === '') {
-      this.setState({
-        participants: this.props.participants,
-      });
-      return;
-    }
-    const participants = this.state.trie.get(searchVal);
-    this.setState({
-      participants,
-    });
+  
   }
 
   render() {
-    let participantsList = this.state.participants.map((p, i) => (
-      <ParticipantCard key={i} participant={p}></ParticipantCard>
-    ));
+    // let participantsList = this.state.participants.map((p, i) => (
+    //   <ParticipantCard key={i} actionItem={p}></ParticipantCard>
+    // ));
 
-    if (this.state.participants.length == 0) {
-      participantsList = <p>There are no participants to show.</p>;
-    }
+    // if (this.state.participants.length == 0) {
+    //   participantsList = <p>There are no participants to show.</p>;
+    // }
 
     return (
       <div className="dashboard">
         <Navbar></Navbar>
         <div className="content">
-          <h1>Participant Dashboard</h1>
+          <h1>Assignment Dashboard</h1>
           <div className="table-container">
-            <div>
-              <div className="search-bar">
-                <InputBase
-                  placeholder="filter participants"
-                  onChange={this.handleChange}
-                />
-                <IconButton type="submit" aria-label="search">
-                  <SearchIcon />
-                </IconButton>
-              </div>
-              <table>
-                <thead>
-                  <tr>
-                    <th>PARTICIPANT</th>
-                    <th>STATUS</th>
-                    <th>PAPERWORK</th>
-                    <th>CASE NOTES</th>
-                    <th>FORM STATUS</th>
-                    <th></th>
-                  </tr>
-                </thead>
-                <tbody>{participantsList}</tbody>
-              </table>
+            <div >
+              <ParticipantCard actionItem = {action}></ParticipantCard>
             </div>
           </div>
         </div>
