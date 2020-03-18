fca8725ea2a546846da005feafd26009f5c61dcb
Julian Kung <julianrkung@berkeley.edu>
2020-03-18 13:46:56 -0700

    Added controllers and basic logic

---
+++ app/controllers/api/action_item_controller.rb
@@ -0,0 +1,19 @@
+class Api::ActionItemController < ApplicationController
+    before_action :set_assignment, only: [:show, :update, :destroy]
+    respond_to :json
+
+    def create
+    end
+
+    def show
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+
+    private
+end
