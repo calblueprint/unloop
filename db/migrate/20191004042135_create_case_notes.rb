87222626d952a867388ab2610b93e7b05a1386a4
Erin <erinsong@berkeley.edu>
2020-03-29 20:03:08 -0700

    added migration changing internal to visible in casenote

--- db/migrate/20191004042135_create_case_notes.rb
+++ db/migrate/20191004042135_create_case_notes.rb
@@ -2,7 +2,7 @@ class CreateCaseNotes < ActiveRecord::Migration[6.0]
   def change
     create_table :case_notes do |t|
       t.string :description
-      t.boolean :visible
+      t.boolean :internal
       t.references :staff, null: false, foreign_key: true
       t.references :participant, null: false, foreign_key: true
       t.string :title
