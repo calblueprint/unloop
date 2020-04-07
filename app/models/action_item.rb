9f3f42def8adcd6d3e5e714fdc16d8c09dfd5e77
Julian Kung <julianrkung@berkeley.edu>
2020-04-06 23:47:42 -0700

    Only be able to delete action items without assignments attached to them

--- app/models/action_item.rb
+++ app/models/action_item.rb
@@ -1,5 +1,5 @@
 class ActionItem < ApplicationRecord
-    has_many :assignments, dependent: :destroy
+    has_many :assignments, dependent: :restrict_with_error
 
     validates :title, :description, presence: true
     validates :is_template, inclusion: [true, false]
