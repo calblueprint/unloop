58cccf656d6ae97b36c294a4df62da1d9dfe470d
Frederick Kim <freddy.kim8@gmail.com>
2020-04-14 23:16:56 -0700

    check if mailing configs breaking prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,20 +60,20 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "unloop_production"
 
-  config.action_mailer.perform_caching = false
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: ENV['BINDING'], protocol: 'https' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    user_name:     ENV['SENDMAIL_USERNAME'],
-    password:      ENV['SENDMAIL_PASSWORD'],
-    domain:        ENV['SMTP_DOMAIN'],
-    address:       "smtp.sendgrid.net",
-    port:          587,
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
+  # config.action_mailer.perform_caching = false
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.default_url_options = { host: ENV['BINDING'], protocol: 'https' }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   user_name:     ENV['SENDMAIL_USERNAME'],
+  #   password:      ENV['SENDMAIL_PASSWORD'],
+  #   domain:        ENV['SMTP_DOMAIN'],
+  #   address:       "smtp.sendgrid.net",
+  #   port:          587,
+  #   authentication: :plain,
+  #   enable_starttls_auto: true
+  # }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
