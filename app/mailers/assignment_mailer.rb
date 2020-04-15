9d023456e557ca2da13430d58b9dd8e385396034
Erin <erinsong@berkeley.edu>
2020-04-15 00:09:15 -0700

    changed default email address

--- app/mailers/assignment_mailer.rb
+++ app/mailers/assignment_mailer.rb
@@ -1,12 +1,12 @@
 class AssignmentMailer < ActionMailer::Base
-    default from: 'no-reply@unloop.org'
+    default from: 'unloopauth@gmail.com'
 
     def new_assignment
         @assignment = params[:assignment]
         if Rails.env.production?
-            @url = ENV['PROD_MAIL_HOST']
+            @url = "https://unloop-staging.herokuapp.com/"
         else
-            @url = ENV['DEV_MAIL_HOST']
+            @url = "localhost:3000"
         end
         mail(to: @assignment.assigned_to.email, subject: '[Unloop] New Action Item Assigned')
     end
