c5eae3fd546c04f6cff5af0224556535459474d9
Frederick Kim <freddy.kim8@gmail.com>
2020-04-14 23:08:01 -0700

    Change host to binding

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: ENV['HOST'], protocol: 'https' }
+  config.action_mailer.default_url_options = { host: ENV['BINDING'], protocol: 'https' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     user_name:     ENV['SENDMAIL_USERNAME'],
