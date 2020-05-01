254707ec9bfac56752e88aafef4f5d43227b1da6
didvi <divi@berkeley.edu>
2020-04-30 18:52:46 -0600

    added back styles

---
+++ app/javascript/components/ActionItemParticipant/styles.js
@@ -0,0 +1,26 @@
+export const styles = () => ({
+  participantSelect: {
+    position: 'absolute',
+    right: '0px',
+    horizontalAlign: 'right',
+    float: 'right',
+  },
+
+  participant: {
+    marginTop: '12px',
+    height: '30px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: '100%',
+  },
+
+  participantBar: {
+    width: '0.4rem',
+    height: '36px',
+    marginRight: '7%',
+    borderRadius: '16px',
+  },
+});
+
+export default styles;
