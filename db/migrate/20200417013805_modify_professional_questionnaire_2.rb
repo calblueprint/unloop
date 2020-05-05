705e493f8017bdaf780923a7a54e1d7933cd18cf
Kyle Hua <kyleqhua@gmail.com>
2020-05-05 02:29:44 -0700

    Got rid of extra migration files

--- db/migrate/20200417013805_modify_professional_questionnaire_2.rb
+++
@@ -1,6 +0,0 @@
-class ModifyProfessionalQuestionnaire < ActiveRecord::Migration[6.0]
-  add_column :professional_questionnaires, :education_history, :string
-  add_column :professional_questionnaires, :begin_skills_assessment_date, :datetime
-  add_column :professional_questionnaires, :end_skills_assessment_date, :datetime
-  add_column :professional_questionnaires, :assigned_mentor, :string
-end
--- db/migrate/20200417021537_remove_unused_prof_questionnaire_fields_2.rb
+++
@@ -1,6 +0,0 @@
-class RemoveUnusedProfQuestionnaireFields < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :professional_questionnaires, :skills_assessment_date, :string
-    remove_column :professional_questionnaires, :mentorship_interest, :string
-  end
-end
