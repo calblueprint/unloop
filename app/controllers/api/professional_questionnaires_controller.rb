c823cd2ed72e35df70faccce3ee93d8cc583989d
Kyle Hua <kyleqhua@gmail.com>
2020-05-15 01:35:09 -0700

    fixed merge errors

--- app/controllers/api/professional_questionnaires_controller.rb
+++ app/controllers/api/professional_questionnaires_controller.rb
@@ -1,34 +1,16 @@
 class Api::ProfessionalQuestionnairesController < ApplicationController
-    before_action :set_questionnaire, only: [:update, :destroy]
-    respond_to :json
-  
-    def create
-      @questionnaire = ProfessionalQuestionnaire.new(questionnaire_params)
-      authorize @questionnaire, policy_class: QuestionnairePolicy
-      sentry_helper(@questionnaire)
-      if @questionnaire.save
-        render json: @questionnaire, status: :created
-      else
-        Raven.capture_message("Could not create professional questionnaire")
-        render json: { error: 'Could not create professional questionnaire' }
-      end
-    end
-  
-    def update
-      authorize @questionnaire, policy_class: QuestionnairePolicy
-      if !questionnaire_params.nil? && questionnaire_params.fetch(:resume).present? && !(questionnaire_params.fetch(:resume).eql?("null"))
-        if @questionnaire.resume.attached?
-          @questionnaire.resume.purge
-        end
-        @questionnaire.resume.attach(questionnaire_params.fetch(:resume))
-      end
-      
-      if @questionnaire.update!(questionnaire_params.except(:resume))
-        render json: @questionnaire, status: :ok
-      else
-        Raven.capture_message("Could not update professional questionnaire")
-        render json: { error: 'Could not update professional questionnaire' }, status: :unprocessable_entity
-      end
+  before_action :set_questionnaire, only: [:update, :destroy]
+  respond_to :json
+
+  def create
+    @questionnaire = ProfessionalQuestionnaire.new(questionnaire_params)
+    authorize @questionnaire, policy_class: QuestionnairePolicy
+    sentry_helper(@questionnaire)
+    if @questionnaire.save
+      render json: @questionnaire, status: :created
+    else
+      Raven.capture_message("Could not create professional questionnaire")
+      render json: { error: 'Could not create professional questionnaire' }
     end
   end
 
@@ -49,17 +31,13 @@ class Api::ProfessionalQuestionnairesController < ApplicationController
     end
   end
 
-    def sentry_helper(professional_questionnaire)
-      Raven.extra_context(professional_questionnaire: professional_questionnaire.attributes)
-      Raven.extra_context(participant: professional_questionnaire.participant.user.attributes)
-    end
-  
-    # may not work
-    def questionnaire_params
-      questionnaire_params = params.require(:professional_questionnaire).permit(:participant_id, :course_completion,
-        :work_history, :job_search_materials, :professional_goals, 
-        :barriers, :education_history, :begin_skills_assessment_date, :end_skills_assessment_date,
-        :assigned_mentor, :success_strategies, :resume)
+  def destroy
+    authorize @questionnaire, policy_class: QuestionnairePolicy
+    if @questionnaire.destroy
+      render json: @questionnaire, status: :ok
+    else
+      Raven.capture_message("Failed to delete professional questionnaire")
+      render json: { error: 'Failed to delete professional questionnaire' }, status: :unprocessable_entity
     end
   end
 
